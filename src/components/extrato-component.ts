1c135ab33d6f41754307eb64563324e55cfc759b
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-02 15:05:41 -0300

    feat: mostrando extratos na tela

---
+++ src/components/extrato-component.ts
@@ -0,0 +1,33 @@
+import Conta from "../types/Conta.js"
+import { FormatoData } from "../types/FormatoData.js"
+import { GrupoTransacao } from "../types/GrupoTransacao.js"
+import { formatarData, formatarMoeda } from "../utils/formatters.js"
+
+const elementoRegistroTransacoesExtrato: HTMLElement = document.querySelector(".extrato .registro-transacoes")
+
+function renderizarExtrato(): void {
+  const grupoTransacoes: GrupoTransacao[] = Conta.getGruposTransacoes()
+  elementoRegistroTransacoesExtrato.innerHTML = ""
+  let htmlRegistroTransacoes: string = ""
+  for (let grupoTransacao of grupoTransacoes) {
+    let htmlTransacaoItem: string = ""
+    for (let transacao of grupoTransacao.transacoes) {
+      htmlTransacaoItem += `
+        <div class="transacao-item">
+          <div class="transacao-info">
+              <span class="tipo">${transacao.tipo}</span>
+              <strong class="valor">${formatarMoeda(transacao.valor)}</strong>
+          </div>
+          <time class="data">${formatarData(transacao.data, FormatoData.DIA_MES)}</time>
+        </div>`
+    }
+    htmlRegistroTransacoes += `<div class="transacoes-group"><strong class="mes-group">${grupoTransacao.label}</strong>${htmlTransacaoItem}</div>`
+  }
+
+  if (htmlRegistroTransacoes === null) {
+    htmlRegistroTransacoes = "`<div>Nada de extrato</div>"
+  }
+  elementoRegistroTransacoesExtrato.innerHTML = htmlRegistroTransacoes
+}
+
+renderizarExtrato()
