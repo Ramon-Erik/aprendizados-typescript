a57f2091c3d03e93f35ae8a32f7dbf52b46a045e
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-14 14:56:25 -0300

    union types

--- dener/array-objeto/index.ts
+++ dener/array-objeto/index.ts
@@ -1,4 +1,5 @@
 const array1: [string, string, number] = ['primeira str', 'segunda str', 3]
+// isso é uma tupla
 // da para definir os valore e a ordem aceitos no array
 // como aqui é especifico os valores, eles são obrigatórios
 // nos outros não, pois um array pode ser vazio
@@ -7,3 +8,4 @@ const array2: Array<string> = ['a']
 // definir somente o tipo generico do array
 
 const array3: Array<string | [] | {nome: string}>  = [{nome: ''}, [], '', '']
+// usado o union types  
