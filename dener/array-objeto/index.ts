485b864dc2ac2d6fa05d826c5ede1e3e08a9bbba
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-08-14 14:50:33 -0300

    outros tipos de dados

---
+++ dener/array-objeto/index.ts
@@ -0,0 +1,9 @@
+const array1: [string, string, number] = ['primeira str', 'segunda str', 3]
+// da para definir os valore e a ordem aceitos no array
+// como aqui é especifico os valores, eles são obrigatórios
+// nos outros não, pois um array pode ser vazio
+
+const array2: Array<string> = ['a']
+// definir somente o tipo generico do array
+
+const array3: Array<string | [] | {nome: string}>  = [{nome: ''}, [], '', '']
---
+++ dener/strings-number-boolean/index.ts
@@ -0,0 +1,10 @@
+// let pedir = "pizza"
+// pedir = 11 não funciona pois ele inicia com tipo string
+
+let pedir: string = "pizza"
+// pedir = 11 não funciona pois está claro que só se aceita string
+
+function falar(frase: string): boolean {
+  console.log(frase);
+  return true
+}
